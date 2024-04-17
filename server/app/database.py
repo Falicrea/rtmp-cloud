8f411d50db6749a53d4c90e75d2aa91f87d3630e
ftiafeno <ftiafeno@inesys.fr>
2024-04-17 17:41:23 +0300

    FIX: Dynamisation de la connection au base de donnée pour la vérification des clés du stream

--- server/app/database.py
+++ server/app/database.py
@@ -2,13 +2,24 @@ from sqlalchemy import create_engine, Column, Boolean, String, Integer, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pydantic import PostgresDsn
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_NAME = os.environ.get('POSTGRES_DB')
+DATABASE_USER = os.environ.get("POSTGRES_USER")
+DATABASE_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+DATABASE_HOST = os.environ.get("POSTGRES_HOST")
+
+print(DATABASE_NAME)
 
 SQLALCHEMY_DATABASE_URL = PostgresDsn.build(
     scheme="postgresql",
-    user="",
-    password="",
-    host="",
-    path=f"/{'' or ''}",
+    user=DATABASE_USER,
+    password=DATABASE_PASSWORD,
+    host=DATABASE_HOST,
+    path=f"/{DATABASE_NAME or ''}",
 )
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
