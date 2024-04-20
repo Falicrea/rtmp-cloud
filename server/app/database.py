eed76383412467f5bc2279ee8f266bc1d26e27fb
Tiafeno Finel <tiafenofnel@gmail.com>
2024-04-20 15:32:58 +0300

    FIX: Intégration de gestion de base de donne multiple pour chaque environnement.

--- server/app/database.py
+++ server/app/database.py
@@ -1,28 +1,53 @@
 from sqlalchemy import create_engine, Column, Boolean, String, Integer, Date
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from pydantic import PostgresDsn
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-DATABASE_NAME = os.environ.get('POSTGRES_DB')
-DATABASE_USER = os.environ.get("POSTGRES_USER")
-DATABASE_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
-DATABASE_HOST = os.environ.get("POSTGRES_HOST")
+DEFAULT_CONNECTION = os.environ.get("DEFAULT_ENGINE") or 'bardv'
 
-SQLALCHEMY_DATABASE_URL = PostgresDsn.build(
-    scheme="postgresql",
-    user=DATABASE_USER,
-    password=DATABASE_PASSWORD,
-    host=DATABASE_HOST,
-    path=f"/{DATABASE_NAME or ''}",
-)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Function to create a new engine and add it to the engines dictionary
+engines_dict = {}
+
+
+def add_engine(engine_name: str, db_user: str, db_pass: str, db_host: str, db_name: str):
+    DATABASE_NAME = os.environ.get(db_name)
+    DATABASE_USER = os.environ.get(db_user)
+    DATABASE_PASSWORD = os.environ.get(db_pass)
+    DATABASE_HOST = os.environ.get(db_host)
+
+    engine = create_engine(PostgresDsn.build(
+        scheme="postgresql",
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD,
+        host=DATABASE_HOST,
+        path=f"/{DATABASE_NAME or ''}",
+    ))
+    engines_dict[engine_name] = engine
+
+# Environnement de dev
+add_engine('bardv',
+           'POSTGRES_USER_BARD_DEV',
+           'POSTGRES_PASSWORD_BARD_DEV',
+           'POSTGRES_HOST_BARD_DEV',
+           'POSTGRES_DB_BARD_DEV'
+           )
+
+
+def get_db(engine_name: str = DEFAULT_CONNECTION) -> Session:
+    engine = engines_dict[engine_name]
+    SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+    return SessionLocal()
+
+def dbs():
+    return engines_dict
+
 
 Base = declarative_base()
+
 
 class Streaming(Base):
     __tablename__ = "Streaming"
@@ -30,6 +55,9 @@ class Streaming(Base):
     id = Column(Integer, primary_key=True)
     idStream = Column(String, unique=True, index=True)
     live = Column(Boolean)
+    flvsUrl = Column(String, default=None)
+    m3u8Url = Column(String, default=None)
+    recorded = Column(Boolean, default=False)
     name = Column(String)
     description = Column(String)
     deleted = Column(Integer, default=0)
--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,11 @@
-from typing import Union
+from typing import Union, Annotated
 from sqlalchemy.orm import Session
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI, Depends, HTTPException
 import logging
 import sys
 import os
-from .database import SessionLocal, Streaming
+from .database import get_db, dbs, Streaming
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -15,19 +15,21 @@ logger.addHandler(stream_handler)
 
 app = FastAPI()
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+
+async def bind_session(name: Union[str, None]) -> Session | None:
+    engine_key = name.split('_')[0]
+    if engine_key not in dbs().keys():
+        return None
+    return get_db(engine_key)
+
 
 @app.get("/auth")
-async def auth(name: Union[str, None] = None, db: Session = Depends(get_db)):
-    if name is None:
+async def auth(name: Union[str, None] = None, db: Union[Annotated[Session, None], None] = Depends(bind_session)):
+    if name is None or db is None:
         raise HTTPException(status_code=422, detail="Parameter invalid")
-    stream = db.query(Streaming).filter(Streaming.idStream == name).first()
+    stream = (db.query(Streaming)
+              .filter(Streaming.idStream == name)
+              .first())
     if stream is None:
         raise HTTPException(status_code=422, detail="Stream not found")
     return JSONResponse(
@@ -35,12 +37,30 @@ async def auth(name: Union[str, None] = None, db: Session = Depends(get_db)):
         content={"success": True}
     )
 
+
 @app.get("/end")
-async def auth(name: Union[str, None] = None, db: Session = Depends(get_db)):
-    if name is None:
+async def ended(name: Union[str, None] = None, db: Union[Annotated[Session, None], None] = Depends(bind_session)):
+    if name is None or db is None:
         raise HTTPException(status_code=422, detail="Parameter invalid")
     os.system('chmod -R 775 /mnt')
     os.system(f'echo "#EXT-X-ENDLIST" >> /mnt/hls/{name}/index.m3u8')
+
+    stream = (db.query(Streaming)
+              .filter(Streaming.idStream == name)
+              .first())
+    stream.live = False
+    stream.recorded = True
+    # Get m3u8 and flv file url
+    stream.m3u8Url = f"/hls/{name}/index.m3u8"
+
+    for root, _, files in os.walk("/mnt/recordings"):
+        for file in files:
+            if file.startswith(name):
+                stream.flvsUrl = f"/recordings/{file}"
+                break
+
+    db.commit()
+
     return JSONResponse(
         status_code=200,
         content={"success": True}
