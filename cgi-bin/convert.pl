bc75cf5afcdcffcbc9f8072ed5e1e4f4e1bf55c3
Tiafeno Finel <tiafenofnel@gmail.com>
2024-04-23 20:23:45 +0300

    FIX: Effectuer la convertion du fichier .flv en mp4 en utilisant l'application ffmpeg, dans le script perl

--- cgi-bin/convert.pl
+++ cgi-bin/convert.pl
@@ -3,14 +3,14 @@
 use warnings FATAL => 'all';
 use strict;
 use CGI; # perl -e shell -MCPAN >> install CGI
-
+use File::Basename;
 
 print "Content-type: text/html\n\n";
 
 my $cgi = CGI->new();
 # Retrieve the value of a query parameter named 'path'
 my $flv = $cgi->param('flv');
+my $filename = basename($flv, '.flv');
 #print "The value of the parameter 'path' is: $param_value\n\r";
 
-system qq(ffmpeg -h);
-print( "echo The flv filename is: $flv" );
+system qq(ffmpeg -i /mnt$flv -vcodec libx264 -acodec copy /mnt/mp4s/$filename.mp4);
