a6dd4c27b1c9a1cc5059939b5cef919a8af64051
Tiafeno Finel <tiafenofnel@gmail.com>
2024-05-05 16:26:47 +0300

    Changement de nom de variable

--- server/app/package/database.py
+++ server/app/package/database.py
@@ -10,7 +10,7 @@ from yaml import safe_load, YAMLError
 load_dotenv()
 
 CONFIG_FILE = "./configs/defined.yaml"
-DEFAULT_CONNECTION = ''
+default_connection = ''
 
 # Function to create a new engine and add it to the engines dictionary
 engines_dict = {}
@@ -47,7 +47,7 @@ def add_engine(engine_name: str, db_name: Union[str, None], db_user: str = '', d
         password=db_pass,
         host=db_host,
         path=f"/{db_name or ''}",
-    ))
+    ), pool_timeout=1800)
     engines_dict[engine_name] = engine
 
 
@@ -55,33 +55,34 @@ def add_engine(engine_name: str, db_name: Union[str, None], db_user: str = '', d
 # This block of code is reading a YAML file named "defined.yaml" located in the "./configs" directory.
 # It then loads the content of the YAML file using the `safe_load` function from the `yaml` module.
 def load_engine():
+    global default_connection
     with open(CONFIG_FILE, "r") as stream:
         try:
-            config = safe_load(stream=stream)
+            config: dict = safe_load(stream=stream)
             databases = config.get('database')
-            for database_name in databases.keys():
-                schema: dict = databases.get(database_name)
+            for connection in databases.keys():
+                schema: dict = databases.get(connection)
                 host = schema['dbhost']
                 dbname = schema['dbname']
                 username = schema['dbuser']
                 password = schema['dbpass']
 
                 add_engine(
-                    engine_name=database_name,
+                    engine_name=connection,
                     db_name=dbname,
                     db_user=username,
                     db_pass=password,
                     db_host=host)
 
                 if schema.get('default') and schema['default'] == 1:
-                    DEFAULT_CONNECTION = database_name
+                    default_connection = connection
 
-                print(f"Database engine load: {database_name}")
+                print(f"Database engine load: {connection}")
         except YAMLError as er:
             print(er)
 
 
-def get_db(engine_name: str = DEFAULT_CONNECTION) -> Session:
+def get_db(engine_name: str = default_connection) -> Session:
     """
     The function `get_db` returns a database session based on the specified engine name.
     
